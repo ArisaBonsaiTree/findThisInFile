98515601c4e4f8563fa5a811c7e8be7b494d1eca
Angel <angelevarela@gmail.com>
2023-01-26 22:55:59 -0800

    Now we got all the champs and their cost

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Simple program to
 # https://dotesports.com/tft/news/tft-set-8-cheat-sheet-all-traits-and-stats
 
-exceptions = ["Threat"]
+exceptions = ["Threat", "One-cost", "Two-cost", "Three-cost", "Four-cost", "Five-cost"]
 file_one = open("traits.txt", encoding="utf8")
 lines = file_one.readlines()
 count = 0
@@ -13,3 +13,25 @@ for line in lines:
         print(line)
 
 print(count)
+
+print("=================")
+
+file_two = open("getchamps.txt", encoding="utf8")
+lines = file_two.readlines()
+
+
+# We want to read a file and get all the champs and get their cost
+
+for line in lines:
+
+    line = line.replace("\n", "")
+
+    list = line.split(" ")
+
+    if any(line.startswith(ex) for ex in exceptions):
+        print("=========")
+        print(line)
+
+    if(len(list) == 1 and len(list[0]) > 2):
+        print(list[0])
+        #print(list)
